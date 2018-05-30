c34d2b629b4035c0a88fa6853944a21a9fd68336
jonDorito <jonDorito@users.noreply.github.com>
2018-05-29 23:01:56 -0600

    MAIN y tools
    Se añadio el main y un modulo tools

---
+++ Road-to-Cartaguito-main/MAIN.py
@@ -0,0 +1,16 @@
+import pygame as pg
+import time
+import loader
+import enemy
+import Player
+
+#Colors
+white = (255, 255, 255)
+black = (0, 0, 0)
+red = (255, 0, 0)
+green = (0, 255, 0)
+blue = (0, 0, 255)
+
+
+
+
---
+++ Road-to-Cartaguito-main/Tools.py
@@ -0,0 +1,13 @@
+import pygame as pg
+#Define la superficie y font para el texto
+def text_in_menu(text, color, font):
+    textS = font.render(text, True, color)
+    return textS, textS.get_rect()
+
+
+# Define donde se mostrara el texto y con que color y tamaño
+def mostrarTexto(text, x, y, size, color):
+    TitleText = pg.font.Font(ffx, size)
+    TSurface, Trect = text_in_menu(text, color, TitleText)
+    Trect.center = ((x, y))
+    screen.blit(TSurface, Trect)
