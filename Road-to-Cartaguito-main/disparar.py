60089e58a5a7449e2bc89478a079912171495bff
johnny1297 <32761368+johnny1297@users.noreply.github.com>
2018-05-30 13:00:34 -0600

    Create disparar.py

---
+++ Road-to-Cartaguito-main/disparar.py
@@ -0,0 +1,24 @@
+#cambiar class player
+# def init(self, x, y, angulo, score, img1, img2):
+# def disparar(x, y, angulo, img_bala)
+# comprobar main y Disparar tienen display variables igual
+
+class Disparar(pg.sprite.Sprite):
+         def __init__(self, x, y, angulo, img_bala):
+                  super().__init__()
+                  self.x = x
+                  self.y = y
+                  self.angulo = angulo
+                  self.bala = img_bala
+                  self.hitpoint = [x, y]
+
+         def bala(self):
+                  self
+                  if self.rect.top > displayHeight:
+                           self.kill()
+                 elif self.rect.bottom < 0:
+                           self.kill()
+                 elif self.rect.left > displayWidth:
+                           self.kill()
+                elif self.rect.right < 0:
+                         self.kill()
