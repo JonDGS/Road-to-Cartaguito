ef96dcad12265a283f81f70ac4fa31e05f3a9d71
jonDorito <jonDorito@users.noreply.github.com>
2018-05-29 23:41:16 -0600

    Actualizacion
    JonDorito

--- Road-to-Cartaguito-main/loader.py
+++ Road-to-Cartaguito-main/loader.py
@@ -1,9 +1,11 @@
 import pygame as pg
 
+
 def rotate(image, angle):
     rot = pg.transform.rotate(image, angle)
     return rot
 
+
 def load(image):
-    img = pg.image.load(image)
+    img = pg.image.load(image).convert()
     return img
