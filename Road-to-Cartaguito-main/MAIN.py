2e920e63002a1311dbf53fd2374605ea804b999b
johnny1297 <32761368+johnny1297@users.noreply.github.com>
2018-05-30 12:59:06 -0600

    Update MAIN.py

--- Road-to-Cartaguito-main/MAIN.py
+++ Road-to-Cartaguito-main/MAIN.py
@@ -2,7 +2,7 @@ import pygame as pg
 import time
 import loader
 import enemy
-#import Player
+import Player
 
 #Inicializar pygame
 pg.init()
@@ -18,19 +18,76 @@ red = (255, 0, 0)
 green = (0, 255, 0)
 blue = (0, 0, 255)
 
+fps = 30
+
+#hacemos un grupo de todos los sprites que se actualizan por fotograma
+all_sprites = pygame.sprite.Group()
+#hagregamos los sprites a el grupo
+player = Player()
+        ###corregir clase enemy a Enemy
+enemy = Enemy()
+bullet = Bullet()
+
+all_sprites.add(player)
+all_sprites.add(enemy)
+all_sprites.add(bullet)
+
+#game loop
+running = True
+
+while running:
+         clock.tick(fps)
+         
+         #check events 
+         for event in pg.event.get():
+                  if event.type == pg.QUIT: 
+                           sys.exit()        
+                  elif event.type == pg.KEYDOWN:               
+                           if event.key == pg.K_LEFT:
+                                    pressed_left = True
+                           elif event.key == pg.K_RIGHT:     
+                                    pressed_right = True
+                           elif event.key == pg.K_UP:       
+                                    pressed_up = True
+                           elif event.key == pg.K_DOWN:    
+                                    pressed_down = True
+                  elif event.type == pg.KEYUP:           
+                           if event.key == pg.K_LEFT:
+                                    pressed_left = False
+                           elif event.key == pg.K_RIGHT:
+                                    pressed_right = False
+                           elif event.key == pg.K_UP:        
+                                    pressed_up = False
+                           elif event.key == pg.K_DOWN:     
+                                    pressed_down = False
+
+         if pressed_up and pressed_right:
+                  Player.moveUp(), Player.moveRight()
+         if pressed_up and pressed_left  :
+                  Player.moveUp(), Player.moveLeft()
+         if pressed_down and pressed_right:
+                  Player.moveDown(), Player.moveRight()
+         if pressed_down and pressed_left:
+                  Player.moveDown(), Player.moveLeft()
+         if pressed_up:
+             Player.moveUp()
+         if pressed_down:
+             Player.moveDown()
+         if pressed_left:
+             Player.moveLeft()
+         if pressed_right:
+             Player.moveRight()
+                           
+         #update todo
+         all_sprites.update()
+             
+         #draw en la pantalla
+         all_sprites.draw(gameDisplay)
 
 
-test = True
-while test:
-    pg.
-    img = pg.transform.scale(loader.load('Trump_Test_Image.png'), (S_width, S_height))
-    screen.blit(img, (0, 0))
-    clock.tick(60)
-    pg.display.flip()
 
 
-
-
-
+pg.quit()
+quit()
 
 
