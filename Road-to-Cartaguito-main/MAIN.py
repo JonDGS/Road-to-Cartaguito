545739606a0ad538d366b1ce8aa5c1b334eda1c1
jonDorito <jonDorito@users.noreply.github.com>
2018-05-30 12:28:07 -0600

    Actualizacion
    Modulos
    - MAIN
    - Tools

--- Road-to-Cartaguito-main/MAIN.py
+++ Road-to-Cartaguito-main/MAIN.py
@@ -2,10 +2,14 @@ import pygame as pg
 import time
 import loader
 import enemy
-import Player
+#import Player
 
 #Inicializar pygame
 pg.init()
+clock = pg.time.Clock()
+S_width = 960
+S_height = 720
+screen = pg.display.set_mode((S_width, S_height))
 
 #Colors
 white = (255, 255, 255)
@@ -13,6 +17,19 @@ black = (0, 0, 0)
 red = (255, 0, 0)
 green = (0, 255, 0)
 blue = (0, 0, 255)
+
+
+
+test = True
+while test:
+    pg.
+    img = pg.transform.scale(loader.load('Trump_Test_Image.png'), (S_width, S_height))
+    screen.blit(img, (0, 0))
+    clock.tick(60)
+    pg.display.flip()
+
+
+
 
 
 
--- Road-to-Cartaguito-main/Tools.py
+++ Road-to-Cartaguito-main/Tools.py
@@ -1,6 +1,6 @@
 import pygame as pg
 #Define la superficie y font para el texto
-def text_in_menu(text, color, font):
+def text_rendering(text, color, font):
     textS = font.render(text, True, color)
     return textS, textS.get_rect()
 
@@ -8,6 +8,6 @@ def text_in_menu(text, color, font):
 # Define donde se mostrara el texto y con que color y tamaño
 def mostrarTexto(text, x, y, size, color):
     TitleText = pg.font.Font(ffx, size)
-    TSurface, Trect = text_in_menu(text, color, TitleText)
+    TSurface, Trect = text_rendering(text, color, TitleText)
     Trect.center = ((x, y))
     screen.blit(TSurface, Trect)
