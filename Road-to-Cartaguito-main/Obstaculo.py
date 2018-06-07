c142568a1b500307ce4054935cb5fdc714e809f1
jonDorito <jonDorito@users.noreply.github.com>
2018-06-07 10:46:46 -0600

    Menus y obstaculos

--- Road-to-Cartaguito-main/Obstaculo.py
+++ Road-to-Cartaguito-main/Obstaculo.py
@@ -3,13 +3,20 @@ import loader
 
 
 class Obstaculo(pg.sprite.Sprite):
-    def __init__(self, x, y, image, angle):
+    def __init__(self, x, y, image, angle, width, height, radius, shape):
         super().__init__()
         self.x = x
         self.y = y
         self.image = loader.load(image)
         self.rect = self.image.get_rect()
         self.angle = angle
-
-    def rotar(self):
-        self.image = loader.rotate(self.image, self.angle)
+        self.width = width
+        self.height = height
+        self.radius = radius
+        if shape == 'Rec':
+            self.figure = (self.x, self.y, self.width, self.height)
+        elif shape == 'Cir':
+            self.figure = (self.x, self.y)
+        elif shape == 'Line':
+            self.figure = (
+        
